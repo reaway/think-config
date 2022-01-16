00b2a6ff6502c7700acbfd8617030919ec257a5f
reaway <reaway@foxmail.com>
2022-01-16 17:25:50 +0800

    init

---
+++ src/helper.php
@@ -0,0 +1,30 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2021 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+declare (strict_types=1);
+
+use Think\Component\Config\Facade\Config;
+
+if (!function_exists('config')) {
+    /**
+     * 获取和设置配置参数
+     * @param string|array $name  参数名
+     * @param mixed        $value 参数值
+     * @return mixed
+     */
+    function config($name = '', $value = null)
+    {
+        if (is_array($name)) {
+            return Config::set($name, $value);
+        }
+
+        return 0 === strpos($name, '?') ? Config::has(substr($name, 1)) : Config::get($name, $value);
+    }
+}
